56387eb71aa8dad8b91549f9a39bad704bf30eac
No one knows <lnvvalerie60@gmail.com>
2025-11-27 16:34:31 +0000

    example no.1

---
+++ app/map/page.tsx
@@ -0,0 +1,10 @@
+import CustomMap from "@/components/CustomMap/CustomeMap";
+
+export default function Map() {
+  return (
+    <main className="">
+      <h1 className="text-4xl text-neutral-900 font-bold">Custom Weather Map</h1>
+      <CustomMap />
+    </main>
+  )
+}
